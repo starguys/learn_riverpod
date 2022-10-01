79e478be75a83853dac22ccd86ff618f99f156a6
starguys <kjj9500@naver.com>
2022-10-01 14:44:49 +0900

    learn flutter

---
+++ lib/screen/future_provider_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:learn_riverpod/layout/default_layout.dart';
+import 'package:learn_riverpod/riverpod/future_provider.dart';
+
+class FutureProviderScreen extends ConsumerWidget {
+  const FutureProviderScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(multiplesFutureProvider);
+
+    return DefaultLayout(
+      title: 'FutureProviderScreen',
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          state.when(
+            data: (data) {
+              return Text(
+                data.toString(),
+                textAlign: TextAlign.center,
+              );
+            },
+            error: (err, stack) => Text(
+              err.toString(),
+            ),
+            loading: () => Center(
+              child: CircularProgressIndicator(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:learn_riverpod/layout/default_layout.dart';
+import 'package:learn_riverpod/screen/auto_dispose_modifier_screen.dart';
+import 'package:learn_riverpod/screen/family_modifier_screen.dart';
+import 'package:learn_riverpod/screen/future_provider_screen.dart';
+import 'package:learn_riverpod/screen/listen_provider_screen.dart';
+import 'package:learn_riverpod/screen/provider_screen.dart';
+import 'package:learn_riverpod/screen/select_provider_screen.dart';
 import 'package:learn_riverpod/screen/state_notifier_provider_screen.dart';
 import 'package:learn_riverpod/screen/state_provider_screen.dart';
+import 'package:learn_riverpod/screen/stream_provider_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -31,6 +38,76 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('StateNotifierProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => FutureProviderScreen(),
+                ),
+              );
+            },
+            child: Text('FutureProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => StreamProviderScreen(),
+                ),
+              );
+            },
+            child: Text('StreamProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => FamilyModifierScreen(),
+                ),
+              );
+            },
+            child: Text('FamilyModifierScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => AutoDisposeModifierScreen(),
+                ),
+              );
+            },
+            child: Text('AutoDisposeModifierScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => ListenProviderScreen(),
+                ),
+              );
+            },
+            child: Text('ListenProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => SelectProviderScreen(),
+                ),
+              );
+            },
+            child: Text('SelectProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => ProviderScreen(),
+                ),
+              );
+            },
+            child: Text('ProviderScreen'),
           ),
         ],
       ),

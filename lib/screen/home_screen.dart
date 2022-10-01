0b4fdf38b21324ed4f16a295342369da3851b844
starguys <kjj9500@naver.com>
2022-10-01 09:27:24 +0900

    learn flutter

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:learn_riverpod/layout/default_layout.dart';
+import 'package:learn_riverpod/screen/state_notifier_provider_screen.dart';
+import 'package:learn_riverpod/screen/state_provider_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      title: 'HomeScreen',
+      body: ListView(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => StateProviderScreen(),
+                ),
+              );
+            },
+            child: Text('StatesProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => StateNotifierProviderScreen(),
+                ),
+              );
+            },
+            child: Text('StateNotifierProviderScreen'),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screen/state_provider_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:learn_riverpod/layout/default_layout.dart';
+import 'package:learn_riverpod/riverpod/state_provider_screen.dart';
+
+class StateProviderScreen extends ConsumerWidget {
+  const StateProviderScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int provider = ref.watch(numberProvider);
+    return DefaultLayout(
+      title: 'StateProviderScreenm',
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              provider.toString(),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(numberProvider.notifier).update((state) => state + 1);
+              },
+              child: Text(
+                'Up',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(numberProvider.notifier).state =
+                    ref.read(numberProvider.notifier).state - 1;
+              },
+              child: Text(
+                'Down',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (_) => _NextScreen()));
+              },
+              child: Text(
+                'Next Screen ',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _NextScreen extends ConsumerWidget {
+  const _NextScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int provider = ref.watch(numberProvider);
+
+    return DefaultLayout(
+      title: 'StateProviderScreenm',
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              provider.toString(),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(numberProvider.notifier).update((state) => state + 1);
+              },
+              child: Text(
+                'Up',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

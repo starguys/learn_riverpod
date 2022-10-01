8fc21f800ec867ecc2386bcf30ef28b050ed5d5e
starguys <kjj9500@naver.com>
2022-10-01 14:44:42 +0900

    learn flutter

---
+++ lib/screen/family_modifier_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:learn_riverpod/layout/default_layout.dart';
+import 'package:learn_riverpod/riverpod/family_modifier_provider.dart';
+
+class FamilyModifierScreen extends ConsumerWidget {
+  const FamilyModifierScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(familyModifierProvider(5));
+    return DefaultLayout(
+      title: 'FamilyModifierScreen',
+      body: Center(
+        child: state.when(
+          data: (data) => Text(data.toString()),
+          error: (err, stack) => Text(err.toString()),
+          loading: () => CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
